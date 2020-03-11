2b9dafb4a08819375e64d9f1f06251065a082075
boris <borisgorelovspb@gmail.com>
2020-03-11 20:34:13 +0300

    intermediate working version

--- src/myproject/client.c
+++ src/myproject/client.c
@@ -1,12 +1,14 @@
+//usage: ./client [-d <ip>] [-p <port>] file[s]
 #include <netdb.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
 #include <sys/socket.h> 
 #include <stdbool.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 #define LEN 70 
 #define FLEN 256
-#define PORT 1234  
 
 void get_answer(int sockfd)
 {
@@ -37,11 +39,6 @@ int send_file(int sockfd, char* name)
 
     return 0;
 }
-
-// int send_flags()
-// {
-
-// }
 
 int send_name(int sockfd, char* name)
 {
@@ -79,41 +76,128 @@ int send_name(int sockfd, char* name)
     get_answer(sockfd);
     return 0;
 }
-  
-int main() 
+
+int get_errors(int sockfd)
+{
+    FILE* file_ptr = fopen("received_errors", "w"); 
+    char fbuff[FLEN];
+
+    if(file_ptr == NULL)
+    {
+        fputs("unable to open the file with errors\n", stderr);
+        write(sockfd, "unable to open the file with errors\n", sizeof("unable to open the file with errors\n"));
+        return 1;
+    }
+
+    //recieving file
+    read(sockfd, fbuff, sizeof(fbuff));
+    while(strncmp("^^^^^", fbuff, 5) != 0)
+    {
+        fputs(fbuff, file_ptr);
+        read(sockfd, fbuff, sizeof(fbuff));
+    }
+    fclose(file_ptr);
+    return 0;
+}
+
+int print_errors(int sockfd)
+{
+    FILE* file_ptr = fopen("received_errors", "r");
+    long size;
+    char fbuff[FLEN];
+    if (file_ptr == NULL)
+    {
+        fprintf(stderr, "unable to open the file with received errors\n");
+        return 1;
+    }
+
+    //check if file with errors is empty
+    fseek(file_ptr, 0, SEEK_END);
+    size = ftell(file_ptr);
+    if(size == 0)
+        return 0;
+
+    rewind(file_ptr);
+    printf("file with errors was recieved.\nerrors:\n");
+    while(fgets(fbuff, sizeof(fbuff), file_ptr))
+        fputs(fbuff, stdout);
+
+    write(sockfd, "file with errors was recieved\n", sizeof("file with errors was recieved\n"));
+    fclose(file_ptr);
+    return 0;
+}
+
+int main(int argc, char** argv) 
 { 
-    int sockfd; 
+    int sockfd, i, check; 
     struct sockaddr_in servaddr; 
     char file_name[LEN];
-  
+    long int PORT = 1234;
+    char IP[LEN] = "127.0.0.1";
+
+    //check if user want to change default ip or port
+    for (i = 1; i < argc; ++i)
+    {
+        if(strncmp(argv[i], "-d", 2) == 0 && i < argc - 1)
+        {
+            check = snprintf(IP, 16, "%s", argv[i+1]);
+            if(check <= 0 || check >= 16)
+            {
+                fprintf(stderr, "fatal error. wrong ip.\n");
+                return 2;
+            }
+        }
+        if(strncmp(argv[i], "-p", 2) == 0 && i < argc - 1)
+        {
+            PORT = strtol(argv[i+1], NULL, 10);
+            if(PORT < 0 || PORT > 65535)
+            {
+                fprintf(stderr, "fatal error. wrong port.\n");
+                return 2;
+            }
+        }
+        
+    }
+
+    // socket creation and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) 
+    if (sockfd < 0) 
     { 
-        printf("socket creation failed...\n"); 
-        return 1;
+        fprintf(stderr, "fatal error: socket creation failed.\n"); 
+        return 3;
     } 
     else
         printf("Socket successfully created, socket = %d\n", sockfd); 
   
-
+    bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_port = htons(PORT);
+    if(inet_pton(AF_INET, IP, &servaddr.sin_addr.s_addr) <= 0)
+    {
+        fprintf(stderr, "fatal error: inet_pton error for %s\n", IP);
+        return 4;
+    }     
   
     // connect the client socket to server socket 
-    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) 
+    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) 
     { 
-        printf("connection with the server failed...\n"); 
-        return 2;
+        fprintf(stderr, "connection with the server failed...\n"); 
+        return 5;
     } 
     else
-        printf("connected to the server..\n"); 
+        printf("connected to the server.\n"); 
    
     if (send_name(sockfd, file_name) != 0) 
-        return 1;
+        return 6;
 
     if (send_file(sockfd, file_name) != 0)
-        return 1;
+        return 6;
+
+    if (get_errors(sockfd) != 0)
+        return 6;
+
+    if (print_errors(sockfd) != 0)
+        return 6;
 
     close(sockfd); 
 
--- src/myproject/server.c
+++ src/myproject/server.c
@@ -1,3 +1,4 @@
+//usage: ./server [-p <port>]
 #include <stdio.h> 
 #include <netdb.h> 
 #include <netinet/in.h> 
@@ -6,10 +7,11 @@
 #include <sys/socket.h> 
 #include <sys/types.h>
 #include <stdbool.h> 
+#include <unistd.h>
+#include <arpa/inet.h>
 #define LEN 80 
 #define FLEN 256
-#define PORT 1234  
-
+#define LISTEN_BACKLOG 50
 
 int get_name(int sockfd, char* serv_name)
 {
@@ -36,13 +38,14 @@ int get_file(int sockfd, char* serv_name)
     if(file_ptr == NULL)
     {
         fputs("unable to open the file\n", stderr);
+        write(sockfd, "unable to open the file\n", sizeof("unable to open the file\n"));
         return 1;
     }
     printf("file was opened successfully\n");
 
     //recieving file
     read(sockfd, fbuff, sizeof(fbuff));
-    while(strncmp("^^^^^", fbuff, 5))
+    while(strncmp("^^^^^", fbuff, 5) != 0)
     {
         fputs(fbuff, file_ptr);
         read(sockfd, fbuff, sizeof(fbuff));
@@ -57,72 +60,142 @@ int get_file(int sockfd, char* serv_name)
 
 // }
 
-void compile(int sockfd, char* serv_name)
+void get_answer(int sockfd)
+{
+    char ans[FLEN]; 
+    read(sockfd, ans, sizeof(ans)); 
+    printf("From Client: %s\n", ans);
+}
+
+int send_errors(int sockfd)
+{
+    FILE* file_ptr = fopen("errors", "r");
+    char fbuff[FLEN];
+
+    if(file_ptr == NULL)
+    {
+        fputs("unable to open the file with errors\n", stderr);
+        return 1;
+    }
+
+    while(fgets(fbuff, sizeof(fbuff), file_ptr))
+        write(sockfd, fbuff, sizeof(fbuff));
+        
+    write(sockfd, "^^^^^", sizeof("^^^^^"));
+    fclose(file_ptr);
+
+    printf("File with errors has been sent\n");
+    get_answer(sockfd);
+
+    return 0;
+}
+
+int compile(int sockfd, char* serv_name)
 {
     int status;
+    int cx;
     char ans[FLEN];
-    char command[FLEN] = {"gcc -o out "};
-    strcat(command, serv_name);
+    char command[FLEN] = "gcc -o out ";
+    cx = snprintf(command+strlen(command), FLEN - strlen(command), "%s ", serv_name);
+    if (cx <= 0 || cx > FLEN - strlen(command))
+    {
+        fprintf(stderr, "an error occurred while creating an executable command\n");
+        sprintf(ans, "an error occurred while creating an executable command\n");
+        write(sockfd, ans, sizeof(ans));
+        return 1;
+    }
+
+    cx = snprintf(command+strlen(command), FLEN - strlen(command), "> errors 2>&1");
+    if (cx <= 0 || cx > FLEN - strlen(command))
+    {
+        fprintf(stderr, "an error occurred while creating an executable command\n");
+        sprintf(ans, "an error occurred while creating an executable command\n");
+        write(sockfd, ans, sizeof(ans));
+        return 1;
+    }
 
     //compile and send result
     status = system(command);
-    printf("status of compiling: %d\n", status);
-    sprintf(ans, "status of compiling: %d\n", status);
-    write(sockfd, ans, sizeof(ans));
+    if (status == 0)
+    {
+        write(sockfd, "compilation complited successfully\n", sizeof("compilation complited successfully\n"));
+        printf("compilation complited successfully\n");
+        send_errors(sockfd);
+        return 0;
+    }
+
+    write(sockfd, "compilation failed\n", sizeof("compilation failed\n"));
+    printf("compilation failed\n");
+    send_errors(sockfd);
+    return 0;
 }
+
   
-int main() 
+int main(int argc, char** argv) 
 { 
+    long int PORT = 1234;
     int sockfd, connfd; 
     struct sockaddr_in servaddr; 
     char serv_name[FLEN] = "serv_";
+
+    printf("hi1\n");
+
+    if (argc > 1 && strncmp(argv[1], "-p", 2) == 0)
+        PORT = strtol(argv[2], NULL, 10);
   
-    // socket create and verification 
+    // socket creation and verification 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
+    if (sockfd < 0) 
+    { 
+        fprintf(stderr, "fatal error: socket creation failed.\n"); 
+        return 1; 
     } 
     else
-        printf("Socket successfully created..\n");  
-  
+        printf("Socket successfully created.\n");  
+
     // assign IP, PORT 
+    bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
     servaddr.sin_port = htons(PORT); 
   
     // Binding newly created socket to given IP and verification 
-    if (bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) { 
-        printf("socket bind failed...\n"); 
-        exit(0); 
+    if (bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1) 
+    { 
+        fprintf(stderr, "fatal error: socket bind failed.\n"); 
+        return 2; 
     } 
     else
-        printf("Socket successfully binded..\n"); 
+        printf("Socket successfully binded.\n"); 
   
     // Now server is ready to listen and verification 
-    if (listen(sockfd, 5)) { 
-        printf("Listen failed...\n"); 
-        exit(0); 
+    if (listen(sockfd, LISTEN_BACKLOG) == -1) 
+    { 
+        fprintf(stderr, "fatal error: Listen failed.\n"); 
+        return 3; 
     } 
     else
-        printf("Server listening..\n"); 
+        printf("Server listening.\n"); 
   
     // Accept the data packet from client and verification
     connfd = accept(sockfd, NULL, NULL); 
-    if (connfd < 0) { 
-        printf("server acccept failed...\n"); 
-        exit(0); 
+    if (connfd == -1) 
+    { 
+        fprintf(stderr, "fatal error: server acccept failed.\n"); 
+        return 4; 
     } 
     else
-        printf("server acccept the client...\n"); 
+        printf("server acccept the client.\n"); 
   
-    if(get_name(connfd, serv_name) != 0)
-        return 1;
+    if (get_name(connfd, serv_name) != 0)
+        return 5;
 
-    if(get_file(connfd, serv_name) != 0)
-        return 1;
+    if (get_file(connfd, serv_name) != 0)
+        return 5;
 
-    compile(connfd, serv_name);
+    if (compile(connfd, serv_name) != 0)
+        return 5;
 
     close(sockfd); 
+    return 0;
 } 
